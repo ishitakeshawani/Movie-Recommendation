13c521e92c46504ad3ae24179e490d7523b0e2ee
Ishita Keshawani <50510726+ishitakeshawani@users.noreply.github.com>
2021-08-18 16:50:20 +0530

    updated movies with rate and descriptions

--- src/App.js
+++ src/App.js
@@ -4,28 +4,73 @@ import { useState } from "react";
 
 const movies = {
   action: [
-    { name: "URI: The Surgical Strike ", year: "2016" },
-    { name: "Dishoom ", year: "2016" }
-  ],
-
-  comedy: [
     {
-      name: "3 Idiots ",
-      year: "2009"
+      name: "Godzilla: King of the Monsters",
+      year: "2019",
+      rate: "13+",
+      desc:
+        "When rogue scientists set out to reset the balance of humanity by awakening the world's monsters, Godzilla must rise to fend off these chaotic titans."
     },
     {
-      name: "Ajab Prem Ki Ghazab Kahani",
-      year: "2009"
+      name: "Extraction",
+      year: "2020",
+      rate: "18+",
+      desc:
+        "A hardened mercenary's mission becomes a soul-searching race to survive when he's sent into Bangladesh to rescue a drug lord's kidnapped son."
+    },
+    {
+      name: "John Wick",
+      year: "2014",
+      rate: "18+",
+      desc:
+        "When a gangster's son steals his car and kills his dog, fearless ex-hit man John Wick takes on the entire mob to get his revenge."
+    }
+  ],
+
+  SciFiFantasy: [
+    {
+      name: "Jurassic World",
+      year: "2015",
+      rate: "16+",
+      desc:
+        "The owners of a dinosaur theme park try to attract tourists with a thrilling new exhibit, but a deadly giant breaks loose and terrorizes the island."
+    },
+    {
+      name: "Interstellar",
+      year: "2014",
+      rate: "13+",
+      desc:
+        "With humanity teetering on the brink of extinction, a group of astronauts travels through a wormhole in search of another inhabitable planet."
+    },
+    {
+      name: "Jumanji: Welcome to the Jungle",
+      year: "2017",
+      rate: "13+",
+      desc:
+        "Four high school students get sucked into the jungle setting of a video game, where they embark on a quest as their comically mismatched adult avatars."
     }
   ],
   horror: [
     {
-      name: "1920",
-      year: "2008"
+      name: "Alive",
+      year: "2020",
+      rate: "16+",
+      desc:
+        "As a grisly virus rampages a city, a lone man stays locked inside his apartment, digitally cut off from seeking help and desperate to find a way out."
     },
     {
-      name: "Bhoot",
-      year: "2020"
+      name: "Roohi",
+      year: "2021",
+      rate: "16+",
+      desc:
+        "Hired to kidnap a bride, two bumbling pals face a wacky predicament when one falls for their abductee and the other, for the spirit possessing her."
+    },
+    {
+      name: "Annabelle Comes Home",
+      year: "2019",
+      rate: "16+",
+      desc:
+        "A devilish doll presides over a haunted house of horrors, awakening evil spirits in the home of two demonologists and terrorizing their young daughter."
     }
   ]
 };
@@ -37,8 +82,13 @@ export default function App() {
   }
   return (
     <div className="App">
-      <h1> Movie Recommendation </h1>
-      <p style={{ fontSize: "smaller" }}> Checkout some of my favorite ones.</p>
+      <h1 style={{ color: "#e50914" }}> Movie Recommendation </h1>
+      <p style={{ fontSize: "18px", color: "#e3e3e3" }}>
+        {" "}
+        Checkout some of my favorite ones.Movies move us like nothing else can,
+        whether they’re scary, funny, dramatic, romantic or anywhere in-between.
+        So many titles, so much to experience.
+      </p>
 
       <div>
         {Object.keys(movies).map((genre) => (
@@ -47,14 +97,41 @@ export default function App() {
           </button>
         ))}
       </div>
-      <hr />
+      <hr style={{ color: "white" }} />
       <div style={{ textAlign: "left" }}>
         <ul style={{ paddingInlineStart: "0" }}>
           {movies[selectedGenre].map((movie) => (
             <li key={movie.name} className="card">
               {" "}
-              <div style={{ fontSize: "larger" }}> {movie.name} </div>
-              <div style={{ fontSize: "smaller" }}> {movie.year} </div>
+              <div style={{ fontSize: "larger", color: "#e50914" }}>
+                {" "}
+                {movie.name}{" "}
+              </div>
+              <div style={{ fontSize: "smaller", display: "inline-block" }}>
+                {" "}
+                {movie.year}{" "}
+              </div>{" "}
+              &nbsp;
+              <div
+                style={{
+                  fontSize: "smaller",
+                  display: "inline-block",
+                  border: "1px solid white"
+                }}
+              >
+                {" "}
+                {movie.rate}{" "}
+              </div>
+              <div
+                style={{
+                  fontSize: "smaller",
+                  padding: "4px",
+                  fontSize: "15px"
+                }}
+              >
+                {" "}
+                {movie.desc}{" "}
+              </div>
             </li>
           ))}
         </ul>
